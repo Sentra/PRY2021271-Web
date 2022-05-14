e24052b2beff659c1ee3637e416f1d1404d9c6cc
Osca <osca-ru@hotmail.com>
2022-05-13 19:43:09 -0500

    remove

--- Web/src/proxies/auth-header.js
+++
@@ -1,9 +0,0 @@
-export default function authHeader() {
-    let user = JSON.parse(sessionStorage.getItem('user'));
-
-    if (user && user.token) {
-        return { 'Authorization': `Bearer ${user.token}`};
-    } else {
-        return {};
-    }
-}
